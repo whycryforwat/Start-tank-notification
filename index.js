87e9a5a5bafbc440584e6bcea82fd7dabd64695e
whyyyyyyyyyyyyyyyyy <58635858+whycryforwat@users.noreply.github.com>
2023-05-05 02:46:15 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+module.exports = function OnslaughtSound(mod) {
+  mod.hook("S_ACTION_STAGE", 9, (event) => {
+    if (event.skill.id === 70300) {
+      mod.send("S_CUSTOM_STYLE_SYSTEM_MESSAGE", 1, {
+        message: "Guardian Shout pressed!",
+        style: 54,
+      });
+
+      mod.send("S_PLAY_SOUND", 1, {
+        SoundID: 2023,
+      });
+	}  
+    if (event.skill.id === 210102) {
+      mod.send("S_CUSTOM_STYLE_SYSTEM_MESSAGE", 1, {
+        message: "Mounting Rage pressed!",
+        style: 54,
+      });
+
+      mod.send("S_PLAY_SOUND", 1, {
+        SoundID: 2023,
+      });
+	}
+    if (event.skill.id === 30201) {
+      mod.send("S_CUSTOM_STYLE_SYSTEM_MESSAGE", 1, {
+        message: "Divine Wrath pressed!",
+        style: 54,
+      });
+
+      mod.send("S_PLAY_SOUND", 1, {
+        SoundID: 2023,
+      });
+    }
+  });
+};
